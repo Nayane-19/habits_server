1ee342537fa42664691fae0dfa77363f3bd2370d
nayane.santos <nayane.santos@globalsys.com.br>
2023-01-20 21:53:26 -0300

    create routes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
  import Fastify from "fastify";
  import cors from "@fastify/cors";
- import { PrismaClient } from '@prisma/client';
+import { appRoutes } from "./routes";
 
  const app = Fastify()
- const prisma = new PrismaClient()
 
  app.register(cors, {
    origin: ["http://127.0.0.1:5173/"]
  })
-
- app.get('/', async () => {
-   const habits = prisma.habit.findMany()
-    return habits
- })
+ app.register(appRoutes)
 
  app.listen({
     port: 3333,
